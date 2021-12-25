534a7a86b9fd05d26b8f97fb01881c75b6cc6aa0
Kritika <kritikarvin2308@gmail.com>
2021-12-25 18:03:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-projects
+ List of python projects from beginners to advanced
---
+++ madlibs/madlibs.py
@@ -0,0 +1,11 @@
+
+
+adjective = input("Adjective: ");
+verb = input("verb: ");
+verb2 = input("verb1: ");
+famous_person = input("famous_person: ");
+
+madlib = f"Computer programming is {adjective}!!. I love to read and {verb}. \
+Stay hyderated because you are {verb2} , just like {famous_person}";
+
+print(madlib);
